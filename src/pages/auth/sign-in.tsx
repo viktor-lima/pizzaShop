6dc60693bd7777ad123c5409eb335079864a1758
viktor-lima <joaolimak10@outlook.com>
2025-08-03 17:46:29 -0300

    ft: toast (sonner)

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -5,6 +5,7 @@ import { Helmet } from "react-helmet-async";
 
 import { useForm } from "react-hook-form"
 import { z } from "zod"
+import { toast  } from 'sonner'
 
 const signInForm = z.object({
   email: z.string().email(),
@@ -19,6 +20,12 @@ export function SignIn() {
   async function handleSignIn (data: SingInForm) {
     console.log(data)
     await new Promise(resolve => setTimeout(resolve, 2000))
+    toast.success('Enviamos um link de autenticação para seu e-mail.', {
+      action :{
+        label: 'Reenviar',
+        onClick: () => {handleSignIn(data)}
+      }
+    })
   }
 
   return (
