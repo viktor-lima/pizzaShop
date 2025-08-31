917f860a971205aa49d1bf36dd0971b6d4557d6c
viktor-lima <joaolimak10@outlook.com>
2025-08-31 15:54:31 -0300

    ft: filters and pagination

--- src/pages/_layouts/App.tsx
+++ src/pages/_layouts/App.tsx
@@ -2,13 +2,13 @@ import { Header } from "@/components/Header";
 import { Outlet } from "react-router-dom";
 
 export function AppLayout(){
-    return (
-        <div className="flex min-h-screen flex-col antialiased">
-            <Header />
+  return (
+    <div className="flex min-h-screen flex-col antialiased">
+      <Header />
 
-            <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
-                <Outlet />
-            </div>
-        </div>
-    )
+      <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
+        <Outlet />
+      </div>
+    </div>
+  )
 }
---
+++ src/pages/app/orders/OrderTableFilters.tsx
@@ -0,0 +1,41 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Search, X } from "lucide-react";
+
+interface OrderTableFiltersProps {
+  // adicione suas props aqui
+}
+
+export function OrderTableFilters(props: OrderTableFiltersProps) {
+  return (
+    <form className="flex items-center gap-2">
+      <span className="text-sm font-semibold">Filtros</span>
+      <Input placeholder="ID pedido" className="h-8 w-auto" />
+      <Input placeholder="Nome do Cliente" className="h-8 w-[320px]" />
+      <Select defaultValue="all">
+          <SelectTrigger className="h-8 w-[180px]">
+              <SelectValue />
+          </SelectTrigger>
+          <SelectContent>
+              <SelectItem value="all">Todos</SelectItem>
+              <SelectItem value="pending">Pendente</SelectItem>
+              <SelectItem value="canceled">Cancelado</SelectItem>
+              <SelectItem value="processing">Em preparo</SelectItem>
+              <SelectItem value="delivering">Em entrega</SelectItem>
+              <SelectItem value="delivered">Entregue</SelectItem>
+          </SelectContent>
+      </Select>
+
+      <Button type="submit" variant="secondary" size="xs">
+          <Search className="h-4 w-4 mr-2"/>
+          Filtrar Resultados
+      </Button>
+
+      <Button type="button" variant="outline" size="xs">
+          <X className="h-4 w-4 mr-2"/>
+          Remover Filtros
+      </Button>
+    </form>
+  );
+}
